65df97c5f3b5bc5b27868a29f552460809e26772
danadum <119086074+danadum@users.noreply.github.com>
2024-08-08 17:07:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from firebase import firebase
 from main_socket import MainSocket
 from main_socket_e4k import MainSocketE4K
 from bot import Bot
+import time
 
 load_dotenv()
 
@@ -20,6 +21,8 @@ if __name__ == "__main__":
     socket = MainSocket("wss://ep-live-fr1-game.goodgamestudios.com/", base, NOM, MDP)
     Thread(target=socket.run_forever, kwargs={'reconnect': 600}).start()
 
+    time.sleep(5)
+    
     socket_e4k = MainSocketE4K("ws://e4k-live-int4-game.goodgamestudios.com/", base, NOM_E4K, MDP_E4K)
     Thread(target=socket_e4k.run_forever, kwargs={'reconnect': 600}).start()
 
